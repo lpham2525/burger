6e6226064f080d3632e244cc9af0c587df6ee543
LanChi Pham <lpham2525@gmail.com>
2020-05-29 21:14:33 -0700

    made changes so page could render in HTML

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,13 +1,13 @@
 const { Burger } = require('../models/burger.js')
 const router = require('express').Router()
 
-router.get('/', (req, res) => Burger
+router.get('/burgers', (req, res) => Burger
   .selectAll(info => res.json(info)))
 
-router.post('/api/burgers', (req, res) => Burger
+router.post('/burgers', (req, res) => Burger
   .insertOne(req.body, info => res.json(info)))
 
-router.put('/api/burgers/:id', (req, res) => Burger
+router.put('/burgers/:id', (req, res) => Burger
   .updateOne(req.body, { id: req.params.id }, info => res.json(info)))
 
 module.exports = router
--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,6 @@
-module.exports = {
-  Burger: require('./burgers_controller.js')
-}
+const router = require('express').Router()
+
+router.use('/api', require('./burgers_controller.js'))
+router.use(require('./viewRoutes.js'))
+
+module.exports = router
